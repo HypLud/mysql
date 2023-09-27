a804ed69cac4e0fdb9c1521be8a9a84026cbac29
HypLud <hyplud@hotmail.com>
2023-09-27 12:24:12 +0400

    MySQL

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Coucou");
+// Import du paquet MySQL pour pouvoir l'utiliser ici
+import mysql from "mysql"
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'restful_api'
+});
+
+connection.connect((err) => {
+    if (err) throw err
+    console.log('Ok !');
+});
