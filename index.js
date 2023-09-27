fc5fa14e5306d691ea908dd68e916828267de445
HypLud <hyplud@hotmail.com>
2023-09-27 15:35:57 +0400

    Function MySQL

--- index.js
+++ index.js
@@ -9,6 +9,53 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-    if (err) throw err
+    if (err) throw err;
     console.log('Ok !');
 });
+
+function getAllUsers() {
+    connection.query('SELECT * FROM users', (error, results) => {
+        if (error) throw error;
+
+        for (let i = 0; i < results.length; i++) {
+            console.log('#ID : ', results[i].id);
+            console.log('Nom : ', results[i].nom);
+            console.log('Email : ', results[i].email);
+            console.log('Password : ', results[i].password);
+        }
+    });
+
+    connection.end();
+}
+
+/* getAllUsers(); */
+
+
+function getUserId() {
+    connection.query('SELECT id FROM users', (error, results) => {
+        if (error) throw error;
+
+        for (let i = 0; i < results.length; i++) {
+            console.log('#ID : ', results[i].id);
+        }
+    });
+
+    connection.end();
+}
+
+/* getUserId(); */
+
+
+function getUserName() {
+    connection.query('SELECT nom FROM users', (error, results) => {
+        if (error) throw error;
+
+        for (let i = 0; i < results.length; i++) {
+            console.log('Nom : ', results[i].nom);
+        }
+    });
+
+    connection.end();
+}
+
+getUserName();
